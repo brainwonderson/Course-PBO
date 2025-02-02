87b54b5562966dcb8d54c315e4eb058dbd6f580b
Brain Wonderson <112601710+brainwonderson@users.noreply.github.com>
2025-02-02 17:08:25 +0700

    Add files via upload

---
+++ 2324-ge-m01-academic-stats-brainwonderson/src/academic/model/Enrollment.java
@@ -0,0 +1,71 @@
+package academic.model;
+
+/**
+ * @author 12S22035 Brain Wonderson
+ * @author 12S22016 Desri Dabukke
+ */
+
+
+
+public class Enrollment {
+    private String code;
+    private String id;
+    private String year;
+    private String semester;
+    private String grade;
+
+    // Constructor
+    public Enrollment(String code, String id, String year, String semester) {
+        this.code = code;
+        this.id = id;
+        this.year = year;
+        this.semester = semester;
+    }
+
+    // Getters and setters
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return code + "|" + id + "|" + year + "|" + semester + "|" + grade;
+    }
+}
